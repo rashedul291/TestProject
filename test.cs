4dd81357712ab541e488119a0cb70703c036c006
Rashedul Riyad <rashedulriyad291@gmail.com>
2016-12-13 15:33:37 +0600

    added a file

---
+++ test.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program{
+ static void Main(string[] args){
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+
+
+   Console.WriteLine("Test sum is: " + (a+b));
+   }
+}
